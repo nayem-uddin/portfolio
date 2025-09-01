0db21a7e6b800692d856b277998b03694f24a97e
Nayem Uddin Biswas <mnubpial@gmail.com>
2025-09-01 12:23:01 +0600

    add skill card rotation for touch devices

--- app/ui/skills/skillCard.tsx
+++ app/ui/skills/skillCard.tsx
@@ -5,7 +5,7 @@ import Back from "./card/back";
 export default function SkillCard({ skill }: { skill: Skill }) {
   return (
     <div className="mt-2 h-45 md:h-50 w-35 md:w-45 relative">
-      <div className="absolute size-full hover:rotate-y-180 transform-3d duration-600">
+      <div className="absolute size-full hover:rotate-y-180 transform-3d duration-600 pointer-coarse:focus:rotate-y-180">
         <Front {...skill} />
         <Back {...skill} />
       </div>
