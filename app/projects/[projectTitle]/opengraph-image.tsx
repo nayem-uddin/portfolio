68c3f9d74f952b9102d9a016a756aa115f54e88e
Nayem Uddin Biswas <mnubpial@gmail.com>
2025-09-08 19:53:58 +0600

    rename OG image component function

--- app/projects/[projectTitle]/opengraph-image.tsx
+++ app/projects/[projectTitle]/opengraph-image.tsx
@@ -1,25 +1,31 @@
 import { getProjectDetails } from "@/lib/utils";
 import { Props } from "./page";
 import { ImageResponse } from "next/og";
-import Image from "next/image";
+// import Image from "next/image";
 
 export const alt = "Project Details";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
-export default async function OGImage({ params }: Props) {
+export default async function Image({ params }: Props) {
   const { projectTitle } = await params;
   const { cover, title } = getProjectDetails(projectTitle) || {};
   return new ImageResponse(
     (
       <>
-        {/* <img src={cover} alt={title} width={1200} height={630} loading="eager" /> */}
-        <Image
+        <img
+          src={cover}
+          alt={title}
+          width={1200}
+          height={630}
+          loading="eager"
+        />
+        {/* <Image
           src={cover || ""}
           alt={title || ""}
           width={1200}
           height={630}
           loading="eager"
-        />
+        /> */}
       </>
     )
   );
